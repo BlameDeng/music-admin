f6fd30ae3bc2fccf423b91be7c4fc538a7338da4
BlameDeng <476961779@qq.com>
2018-10-06 22:16:01 +0800

    header基本样式

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,19 +1,131 @@
 <template>
     <div class="header">
-        Header
+        <div class="logo">
+            <x-icon name="logo"></x-icon>
+            <span class="logo-text">后台管理系统</span>
+        </div>
+        <div class="user-bar">
+            <Icon type="md-menu" size="25" style="cursor:pointer;" />
+            <div class="user-info" @click="onUserInfo">
+                <span class="user-name">张某某</span>
+                <img src="../../assets/avatar.jpg" alt="avatar" class="user-avatar">
+                <transition name="fade">
+                    <ul class="popover" v-show="popoverVisible">
+                        <li>我的消息</li>
+                        <li>个人设置</li>
+                        <li>退出登录</li>
+                    </ul>
+                </transition>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+    import xIcon from '../icon.vue'
     export default {
         name: 'Header',
-        components: {},
+        components: { xIcon },
         data() {
-            return {
-
+            return { popoverVisible: false }
+        },
+        methods: {
+            onUserInfo() {
+                this.popoverVisible = !this.popoverVisible;
+            },
+            listenDocument() {
+                this.popoverVisible = false;
             }
+        },
+        watch: {
+            popoverVisible(val) {
+                val ? document.addEventListener('click', this.listenDocument) : document.removeEventListener('click', this.listenDocument);
+            }
+        },
+        beforeDestroy() {
+            document.removeEventListener('click', this.listenDocument);
         },
     }
 </script>
 <style scoped lang="scss">
-    .header {}
+    @import '@/assets/base.scss';
+    .header {
+        background: $p;
+        width: 100%;
+        height: 100%;
+        color: #fff;
+        display: flex;
+        >.logo {
+            width: 250px;
+            height: 100%;
+            border-right: 1px solid $border;
+            font-size: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-shrink: 0;
+            >.logo-text {
+                margin-left: 10px;
+            }
+        }
+        >.user-bar {
+            flex-grow: 1;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 40px 0 20px;
+            >.user-info {
+                display: flex;
+                justify-content: flex-start;
+                align-items: center;
+                cursor: pointer;
+                position: relative;
+                >.user-name {
+                    font-size: 15px;
+                }
+                >.user-avatar {
+                    width: 40px;
+                    height: 40px;
+                    vertical-align: top;
+                    border-radius: 50%;
+                    margin-left: 10px;
+                }
+                >.popover {
+                    position: absolute;
+                    top: 100%;
+                    left: 0;
+                    margin-top: 15px;
+                    width: calc(100% + 10px);
+                    margin-left: -10px;
+                    border: 1px solid $border;
+                    color: $content;
+                    border-radius: 4px;
+                    user-select: none;
+                    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.2);
+                    >li {
+                        padding: 5px 0 5px 10px;
+                        &:hover {
+                            background: $bg;
+                        }
+                        &:first-child {
+                            border-top-left-radius: 4px;
+                            border-top-right-radius: 4px;
+                        }
+                        &:last-child {
+                            border-bottom-left-radius: 4px;
+                            border-bottom-right-radius: 4px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity .3s;
+    }
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+    .fade-enter-to, .fade-leave {
+        opacity: 1;
+    }
 </style>
