ea49824b3a29eba5a747f1a1205417f8ec4f2052
BlameDeng <476961779@qq.com>
2018-10-08 22:19:01 +0800

    歌单编辑页布局

--- src/views/sheet/sheet-edit.vue
+++ src/views/sheet/sheet-edit.vue
@@ -33,27 +33,32 @@
         </div>
         <div class="songs">
             <div class="sheet-songs">
+                <Button type="error" class="button">从歌单移除</Button>
                 <div class="title">
-                    歌曲列表
+                    已有歌曲列表
                 </div>
+
                 <div class="song" v-for="n in 14">
-                    <span class="index">{{n}}</span>
-                    <span class="song-name">22222</span>
-                    <span class="singer">333</span>
-                    <span class="">
-                        <Icon type="md-checkmark" class="icon" /></span>
+                    <p class="index">{{n}}</p>
+                    <p class="song-name">22222</p>
+                    <p class="singer">333</p>
+                    <p class="">
+                        <Icon type="md-checkmark" class="icon" />
+                    </p>
                 </div>
             </div>
             <div class="all-songs">
+                <Button type="success" class="button">添加到歌单</Button>
                 <div class="title">
                     歌曲库
                 </div>
                 <div class="song" v-for="n in 44">
-                    <span class="index">{{n}}</span>
-                    <span class="song-name">222333333333333333333333333333333555553333333355555553333333333333333333333322</span>
-                    <span class="singer">333</span>
-                    <span class="">
-                        <Icon type="md-checkmark" class="icon" /></span>
+                    <p class="index">{{n}}</p>
+                    <p class="song-name">222333333333333333333333333333322</p>
+                    <p class="singer">333</p>
+                    <p class="">
+                        <Icon type="md-checkmark" class="icon" />
+                    </p>
                 </div>
             </div>
         </div>
@@ -61,145 +66,164 @@
 </template>
 
 <script>
-    import { mapState } from "vuex";
-    export default {
-        name: "SheetEdit",
-        data() {
-            return {};
-        },
-        computed: {
-            ...mapState({
-                editingSheet: state => state.sheet.editingSheet
-            })
-        }
-    };
+import { mapState } from "vuex";
+export default {
+  name: "SheetEdit",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState({
+      editingSheet: state => state.sheet.editingSheet
+    })
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    @import "@/assets/base.scss";
-    .sheet-edit {
-        display: flex;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        >.sheet-info {
-            border: 1px solid red;
-            width: 40%;
-            height: 100%;
-            padding: 0 30px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            color: $content;
-            >h3 {
-                margin: 10px 0;
-                text-align: center;
-                font-size: 20px;
-                color: $title;
-            }
-        }
-        >.songs {
-            border: 1px solid green;
-            width: 60%;
-            height: 100%;
-            color: $content;
-            font-size: 12px;
-            >.sheet-songs {
-                height: 30%;
-                border: 1px solid red;
-                padding: 5px 20px;
-                overflow: auto;
-                >.title {
-                    color: $p;
-                }
-                >.song {
-                    display: inline-flex;
-                    align-items: center;
-                    width: 40%;
-                    vertical-align: top;
-                    padding: 3px 0;
-                    >span {
-                        display: inline-flex;
-                        white-space: nowrap;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        margin-right: 5px;
-                        &:first-child {
-                            width: 10%;
-                            color: lighten($content, 30%);
-                        }
-                        &.song-name {
-                            width: 50%;
-                        }
-                        &.singer {
-                            width: 30%;
-                        }
-                        &:last-child {
-                            height: 16px;
-                            width: 16px;
-                            border: 1px solid $border;
-                            border-radius: 4px;
-                            cursor: pointer;
-                            >.icon {
-                                display: none;
-                            }
-                        }
-                        &.select {
-                            >.icon {
-                                display: inline;
-                            }
-                        }
-                    }
-                }
-            }
-            >.all-songs {
-                height: 70%;
-                border: 1px solid black;
-                padding: 5px 20px;
-                overflow: auto;
-                >.title {
-                    color: $p;
-                }
-                >.song {
-                    display: inline-flex;
-                    align-items: center;
-                    width: 40%;
-                    vertical-align: top;
-                    padding: 3px 0;
-                    >span {
-                        display: inline-flex;
-                        white-space: nowrap;
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        margin-right: 5px;
-                        &:first-child {
-                            width: 10%;
-                            color: lighten($content, 30%);
-                        }
-                        &.song-name {
-                            width: 50%;
-                        }
-                        &.singer {
-                            width: 30%;
-                        }
-                        &:last-child {
-                            height: 16px;
-                            width: 16px;
-                            border: 1px solid $border;
-                            border-radius: 4px;
-                            cursor: pointer;
-                            >.icon {
-                                display: none;
-                            }
-                        }
-                        &.select {
-                            >.icon {
-                                display: inline;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+@import "@/assets/base.scss";
+.sheet-edit {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  > .sheet-info {
+    border: 1px solid red;
+    width: 40%;
+    height: 100%;
+    padding: 0 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: $content;
+    > h3 {
+      margin: 10px 0;
+      text-align: center;
+      font-size: 20px;
+      color: $title;
     }
+  }
+  > .songs {
+    border: 1px solid green;
+    width: 60%;
+    height: 100%;
+    color: $content;
+    font-size: 12px;
+    position: relative;
+    > .sheet-songs {
+      height: 30%;
+      border: 1px solid red;
+      padding: 5px 20px;
+      overflow: auto;
+      > .title {
+        color: $p;
+      }
+      > .button {
+        position: absolute;
+        top: 5px;
+        right: 25px;
+        padding: 0 4px;
+      }
+      > .song {
+        display: inline-flex;
+        align-items: center;
+        width: 40%;
+        vertical-align: top;
+        padding: 3px 0;
+        &:hover {
+          background: $bg;
+        }
+        > p {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          margin-right: 5px;
+          &:first-child {
+            width: 10%;
+            color: lighten($content, 30%);
+          }
+          &.song-name {
+            width: 50%;
+          }
+          &.singer {
+            width: 30%;
+          }
+          &:last-child {
+            height: 14px;
+            width: 14px;
+            border: 1px solid $border;
+            border-radius: 3px;
+            cursor: pointer;
+            margin-right: 20px;
+            > .icon {
+              display: none;
+            }
+          }
+          &.select {
+            > .icon {
+              display: inline;
+            }
+          }
+        }
+      }
+    }
+    > .all-songs {
+      height: 70%;
+      border: 1px solid black;
+      padding: 5px 20px;
+      overflow: auto;
+      > .title {
+        color: $p;
+      }
+      > .button {
+        position: absolute;
+        bottom: 5px;
+        right: 25px;
+        padding: 0 4px;
+      }
+      > .song {
+        display: inline-flex;
+        align-items: center;
+        width: 40%;
+        vertical-align: top;
+        padding: 3px 0;
+        &:hover {
+          background: $bg;
+        }
+        > p {
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          margin-right: 5px;
+          &:first-child {
+            width: 10%;
+            color: lighten($content, 30%);
+          }
+          &.song-name {
+            width: 50%;
+          }
+          &.singer {
+            width: 30%;
+          }
+          &:last-child {
+            height: 14px;
+            width: 14px;
+            border: 1px solid $border;
+            border-radius: 3px;
+            cursor: pointer;
+            margin-right: 20px;
+            > .icon {
+              display: none;
+            }
+          }
+          &.select {
+            > .icon {
+              display: inline;
+            }
+          }
+        }
+      }
+    }
+  }
+}
 </style>
